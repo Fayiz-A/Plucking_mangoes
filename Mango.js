6a2f5f40fbe17465506578d9309d48e0fc1e0a58
Fayiz-A <68380206+Fayiz-A@users.noreply.github.com>
2020-08-10 00:07:36 +0530

    added rotation to the mangoes

--- Mango.js
+++ Mango.js
@@ -19,7 +19,12 @@ class Mango extends ChildClass{
     }
 
     display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
         imageMode(CENTER);
-        image(this.image, this.body.position.x, this.body.position.y, this.width, this.height);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
     }
 }
